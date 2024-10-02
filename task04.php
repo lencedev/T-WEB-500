1b39e50f00446daf13498dcf21ee598fcbf9e281
lencedev <owen1.bolling@epitech.eu>
2024-10-02 14:16:32 +0200

    update

--- task04.php
+++ task04.php
@@ -1,9 +1,6 @@
 <?php
 function calc(string $operator, int $nbr1, int $nbr2)
 {
-    if (!$operator)
-        return 1;
-
     switch ($operator) {
         case "*":
             return ($nbr1 * $nbr2);
@@ -12,7 +9,7 @@ function calc(string $operator, int $nbr1, int $nbr2)
         case "-":
             return ($nbr1 - $nbr2);
         case "/":
-            if ($nbr1 == 0 || $nbr2 == 0)
+            if ($nbr2 == 0)
                 return "Cannot divide by 0";
             return ($nbr1 / $nbr2);
         default:
